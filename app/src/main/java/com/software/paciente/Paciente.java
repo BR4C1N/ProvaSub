ebd03fc916544ccb951b7c84764d7a7d0de312b3
Matheus Moreira Mendes <matheus-moreira.ivi75@hotmail.com>
2022-12-01 15:30:23 -0400

    Criação de Entidade

---
+++ app/src/main/java/com/software/paciente/Paciente.java
@@ -0,0 +1,40 @@
+package com.software.paciente;
+
+public class Paciente {
+    private String nome;
+    private String telefone;
+    private String CPF;
+    private String aniversario;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getAniversario() {
+        return aniversario;
+    }
+
+    public void setAniversario(String aniversario) {
+        this.aniversario = aniversario;
+    }
+}
